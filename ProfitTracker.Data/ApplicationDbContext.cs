617fbf1d758339376b0c6eb3cd1656b352fd150e
CHOYLAND-DESKTO\charl <charliehoyland1@gmail.com>
2017-02-18 19:23:32 +0000

    Initial Commit

---
+++ ProfitTracker.Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+using Microsoft.AspNet.Identity.EntityFramework;
+using ProfitTracker.Models.Entities;
+
+namespace ProfitTracker.Models
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+    }
+}
---
+++ ProfitTracker/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ProfitTracker.Startup))]
+namespace ProfitTracker
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
